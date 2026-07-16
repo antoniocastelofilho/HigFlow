c17cb87f1753e9ac95ca0a59c05f5a36e7dba23e
kainaas <165678304+kainaas@users.noreply.github.com>
2026-07-16 16:24:00 -0300

    fixxing existing tutorials

--- higtree/doc/tutoriais/tutorial01-2d.c
+++ higtree/doc/tutoriais/tutorial01-2d.c
@@ -2,21 +2,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#include "mtree.h"
-#include "mtree-io.h"
+#include "higtree.h"
+#include "higtree-io.h"
 
 int main(int argc, char *argv[]) {
 	Point lp, hp;
 	POINT_ASSIGN_SCALAR(lp, -1.0);
 	POINT_ASSIGN_SCALAR(hp, 1.0);
-	mt_cell *root = mt_create_root(lp, hp);
+	hig_cell *root = hig_create_root(lp, hp);
 	int num_cell[DIM];
 	POINT_ASSIGN_SCALAR(num_cell, 2);
-	mt_refine_uniform(root, num_cell);
+	hig_refine_uniform(root, num_cell);
 
-	FILE *fd = fopen("tut01.vtk", "w");
-	mtio_print_in_vtk2d(fd, root);
+	FILE *fd = fopen("VTKS/tut01.vtk", "w");
+	higio_print_in_vtk2d(fd, root);
 	fclose(fd);
 
-	mt_destroy(root);
+	hig_destroy(root);
 }
--- higtree/doc/tutoriais/tutorial02-2d.c
+++ higtree/doc/tutoriais/tutorial02-2d.c
@@ -2,30 +2,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#include "mtree.h"
-#include "mtree-io.h"
+#include "higtree.h"
+#include "higtree-io.h"
 
 int main(int argc, char *argv[]) {
 	Point lp, hp;
 	POINT_ASSIGN_SCALAR(lp, -1.0);
 	POINT_ASSIGN_SCALAR(hp, 1.0);
-	mt_cell *root = mt_create_root(lp, hp);
+	hig_cell *root = hig_create_root(lp, hp);
 	int num_cells[DIM];
         num_cells[0] = 4;
         num_cells[1] = 5;
-	mt_refine_uniform(root, num_cells);
+	hig_refine_uniform(root, num_cells);
 
         Point p;
         p[0] = 0.5;
         p[1] = 0.5;
-        mt_cell *c = mt_get_cell_with_point(root,p);
+        hig_cell *c = hig_get_cell_with_point(root,p);
         num_cells[0] = 2;
         num_cells[1] = 2;
-        mt_refine_uniform(c,num_cells);
+        hig_refine_uniform(c,num_cells);
 
-	FILE *fd = fopen("tut02.vtk", "w");
-	mtio_print_in_vtk2d(fd, root);
+	FILE *fd = fopen("VTKS/tut02.vtk", "w");
+	higio_print_in_vtk2d(fd, root);
 	fclose(fd);
 
-	mt_destroy(root);
+	hig_destroy(root);
 }
